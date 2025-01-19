4f1ac2b27bc62f91cec4d3b93955879b6e96e313
Mohamed Attya <ATTYA@DESKTOP-S6J1JOP>
2025-01-19 16:49:35 +0200

    Genaric SWAP Example01

---
+++ Helper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1_AdvanceOOP
+{
+    static  class   Helper
+    {              //    بس  Method   وممكن يكون علي ال    class   ممكن يكون علي ال  Genaric    ال 
+
+        public static void SWAP <T>(ref T X  , ref T Y  )// T ==> Same Templet in C++ , JaveScript 
+        {
+            Console.WriteLine("=================SWAP=============== ");
+            T Temp = X;
+            X=Y; 
+            Y=Temp;
+        }
+        
+
+
+
+
+
+        
+    /// public static void SWAP (ref object  X  , ref object  Y  )
+    /// {
+    ///     Console.WriteLine("=================SWAP=============== ");
+    ///     object  Temp = X;    // 1-    ودي حاجه مش حلوه طبعا   Boxing and UnBoxing   خلاني استخدم ال  Object  بس ال  
+    ///     X=Y;                 // 2-Type Safe  الي بكتبه مش  Code ال 
+    ///                          // 3-  ممكن يجي ودا مش كويس طبعا   Type بتاعي مفتوح ل اي  Scope   يعني ال Limits   مش  Object   ال 
+    ///     Y = Temp;
+    /// }
+
+
+
+
+
+
+    /// public static void SWAP(ref double X  , ref double  Y  )
+    /// {
+    ///     Console.WriteLine("=================SWAP=============== ");
+    ///     double Temp = X;
+    ///     X=Y; 
+    ///     Y=Temp;
+    /// }
+       
+        
+    /// public static void SWAP(ref Point X, ref Point Y)
+    /// {
+    ///     Console.WriteLine("=================SWAP=============== ");
+    ///     Point Temp = X;
+    ///     X = Y;
+    ///     Y = Temp;
+    /// }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Generic SWAP Example01 
+            /// object  A = 5 ,  B = 10;   
+            /// Console.WriteLine($"A = {A}");
+            /// Console.WriteLine($"B = {B}");
+            /// Helper.SWAP/*<int>*/(ref A,ref B );
+            /// Console.WriteLine($"A = {A}");
+            /// Console.WriteLine($"B = {B}");
+
+            /// double K = 3.3, L = 4.4;
+            /// Console.WriteLine($"K = {K}");
+            /// Console.WriteLine($"L = {L}");
+            /// Helper.SWAP /*<double>*/ (ref  K , ref  L);
+            /// Console.WriteLine($"K = {K}");
+            /// Console.WriteLine($"L = {L}");
+
+
+            /// Point P01 = new Point(10, 20);
+            /// Point P02 = new Point(50, 60);
+            /// Console.WriteLine($"P01={P01}");
+            /// Console.WriteLine($"P02={P02}");
+            /// Helper.SWAP /*<Point>*/(ref P02, ref P01);
+            /// Console.WriteLine($"P01={P01}");
+            /// Console.WriteLine($"P02={P02}");
+
+
+            #endregion
+
+
+
+
         }
     }
 }
