a896fe0df9aa546e32432680bc5bcfba04199b76
Mohamed Attya <ATTYA@DESKTOP-S6J1JOP>
2025-01-19 13:59:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo1_AdvanceOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
